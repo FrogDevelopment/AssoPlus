8d47db5aa4df43b8af7f1a7185398656fd79e087
Le Gall Benoit <legall.benoit@gmail.com>
2015-06-14 16:31:57 +0200

    Ajout

---
+++ src/main/java/fr/frogdevelopment/assoplus/bean/Licence.java
@@ -0,0 +1,97 @@
+/*
+ * Copyright (c) Frog Development 2015.
+ */
+
+package fr.frogdevelopment.assoplus.bean;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Set;
+
+@Entity
+@Table(name = "licence", uniqueConstraints = {
+		@UniqueConstraint(columnNames = "code"),
+		@UniqueConstraint(columnNames = "label")
+})
+public class Licence implements Serializable{
+
+	@Id
+	@Column(name = "id", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "code", unique = true, nullable = false)
+	private String code;
+
+	@Column(name = "label", unique = true, nullable = false)
+	private String label;
+
+	@OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL, mappedBy = "licence")
+	private Set<Option> options;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Set<Option> getOptions() {
+		return options;
+	}
+
+	public void setOptions(Set<Option> options) {
+		this.options = options;
+	}
+
+	@Override
+	public String toString() {
+		return "Licence{" +
+				"id=" + id +
+				", code='" + code + '\'' +
+				", label='" + label + '\'' +
+				", options=" + options +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Licence licence = (Licence) o;
+
+		return new org.apache.commons.lang3.builder.EqualsBuilder()
+				.append(id, licence.id)
+				.append(code, licence.code)
+				.append(label, licence.label)
+				.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new org.apache.commons.lang3.builder.HashCodeBuilder(17, 37)
+				.append(id)
+				.append(code)
+				.append(label)
+				.toHashCode();
+	}
+}
---
+++ src/main/java/fr/frogdevelopment/assoplus/bean/Option.java
@@ -0,0 +1,100 @@
+/*
+ * Copyright (c) Frog Development 2015.
+ */
+
+package fr.frogdevelopment.assoplus.bean;
+
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "option", uniqueConstraints = {
+		@UniqueConstraint(columnNames = "code"),
+		@UniqueConstraint(columnNames = "label")
+})
+public class Option {
+
+	@Id
+	@Column(name = "id", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	@Column(name = "code", unique = true, nullable = false)
+	private String code;
+
+	@Column(name = "label", unique = true, nullable = false)
+	private String label;
+
+	@ManyToOne(fetch = FetchType.LAZY, optional = false)
+	@JoinColumn(name = "licence_id", nullable = false)
+	private Licence licence;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getLabel() {
+		return label;
+	}
+
+	public void setLabel(String label) {
+		this.label = label;
+	}
+
+	public Licence getLicence() {
+		return licence;
+	}
+
+	public void setLicence(Licence licence) {
+		this.licence = licence;
+	}
+
+
+	@Override
+	public String toString() {
+		return "Option{" +
+				"id=" + id +
+				", code='" + code + '\'' +
+				", label='" + label + '\'' +
+				", licence=" + licence +
+				'}';
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if (this == o) return true;
+
+		if (o == null || getClass() != o.getClass()) return false;
+
+		Option option = (Option) o;
+
+		return new EqualsBuilder()
+				.append(id, option.id)
+				.append(code, option.code)
+				.append(label, option.label)
+				.isEquals();
+	}
+
+	@Override
+	public int hashCode() {
+		return new HashCodeBuilder(17, 37)
+				.append(id)
+				.append(code)
+				.append(label)
+				.toHashCode();
+	}
+}
