625fc57b7a244b913785946cce49db8e4685aec2
Le Gall Benoit <legall.benoit@gmail.com>
2015-07-18 12:37:17 +0200

    Saisie date via DatePicker

--- src/main/java/fr/frogdevelopment/assoplus/controller/MembersController.java
+++ src/main/java/fr/frogdevelopment/assoplus/controller/MembersController.java
@@ -4,16 +4,18 @@
 
 package fr.frogdevelopment.assoplus.controller;
 
+import fr.frogdevelopment.assoplus.dto.MemberDto;
+import fr.frogdevelopment.assoplus.service.MembersService;
 import javafx.collections.ObservableList;
-import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
-
 import javafx.stage.FileChooser;
+import javafx.util.StringConverter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,11 +23,10 @@ import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
 
-import fr.frogdevelopment.assoplus.dto.MemberDto;
-import fr.frogdevelopment.assoplus.service.MembersService;
-
 import java.io.File;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ResourceBundle;
 
 @Controller("membersController")
@@ -54,7 +55,7 @@ public class MembersController implements Initializable {
 	@FXML
 	private TextField txtFirstname;
 	@FXML
-	private TextField txtBirthday;
+	private DatePicker dpBirthday;
 	@FXML
 	private TextField txtEmail;
 	@FXML
@@ -85,14 +86,37 @@ public class MembersController implements Initializable {
 		} catch (Exception e) {
 			LOGGER.error("FIXME", e);
 		}
+
+		dpBirthday.setPromptText("jj/mm/aaaa");
+		dpBirthday.setConverter(new StringConverter<LocalDate>() {
+			@Override
+			public String toString(LocalDate date) {
+				if (date != null) {
+					return dateFormatter.format(date);
+				} else {
+					return "";
+				}
+			}
+
+			@Override
+			public LocalDate fromString(String string) {
+				if (string != null && !string.isEmpty()) {
+					return LocalDate.parse(string, dateFormatter);
+				} else {
+					return null;
+				}
+			}
+		});
 	}
+
+	private static final DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 
 	public void saveData() {
 		MemberDto member = new MemberDto();
 		member.setStudentNumber(Integer.parseInt(txtStudentNumber.getText()));
 		member.setLastname(txtLastname.getText());
 		member.setFirstname(txtFirstname.getText());
-		member.setBirthday(txtBirthday.getText());
+		member.setBirthday(dpBirthday.getValue().format(dateFormatter));
 		member.setEmail(txtEmail.getText());
 		member.setLicence(txtLicence.getText());
 		member.setOption(txtOption.getText());
