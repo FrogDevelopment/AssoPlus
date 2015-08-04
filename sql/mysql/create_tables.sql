dc581be63509d5770b60efb55f1955840e4bb148
Le Gall Benoit <legall.benoit@gmail.com>
2015-08-04 22:25:41 +0200

    prise en compte des contraintes

--- sql/mysql/create_tables.sql
+++ sql/mysql/create_tables.sql
@@ -2,27 +2,28 @@ CREATE DATABASE IF NOT EXISTS asso_plus;
 
 CREATE TABLE IF NOT EXISTS asso_plus.licence (
   id INT NOT NULL AUTO_INCREMENT,
-  code VARCHAR(10),
-  label VARCHAR(50),
+  code VARCHAR(10) UNIQUE NOT NULL,
+  label VARCHAR(50) UNIQUE NOT NULL,
   PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS asso_plus.`option` (
   id INT NOT NULL AUTO_INCREMENT,
-  code VARCHAR(10),
-  label VARCHAR(50),
+  code VARCHAR(10) UNIQUE NOT NULL,
+  label VARCHAR(50) UNIQUE NOT NULL,
+  licence_code VARCHAR(10),
   PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS asso_plus.member (
   id INT NOT NULL AUTO_INCREMENT,
-  studentNumber INT,
-  lastname VARCHAR(50),
-  firstname VARCHAR(50),
-  birthday VARCHAR(8),
-  email VARCHAR(100),
-  licence_code VARCHAR(10),
-  option_code VARCHAR(10),
+  studentNumber INT UNIQUE NOT NULL,
+  lastname VARCHAR(50) NOT NULL ,
+  firstname VARCHAR(50) NOT NULL ,
+  birthday VARCHAR(8) NOT NULL ,
+  email VARCHAR(100) NOT NULL ,
+  licence_code VARCHAR(10) NOT NULL ,
+  option_code VARCHAR(10) NOT NULL ,
   phone VARCHAR(14),
   adress VARCHAR(50),
   postal_code VARCHAR(5),
