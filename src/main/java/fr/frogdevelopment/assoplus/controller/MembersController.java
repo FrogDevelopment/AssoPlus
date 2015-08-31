f1f7f9b42b2dd598e90c89c8f56a7ee4f5fa1947
Benoît Le Gall <benoit.legall@bio-c-bon.fr>
2015-08-31 12:50:52 +0200

    new icons

--- src/main/java/fr/frogdevelopment/assoplus/controller/MembersController.java
+++ src/main/java/fr/frogdevelopment/assoplus/controller/MembersController.java
@@ -4,16 +4,17 @@
 
 package fr.frogdevelopment.assoplus.controller;
 
-import fr.frogdevelopment.assoplus.dto.MemberDto;
-import fr.frogdevelopment.assoplus.service.MembersService;
-import fr.frogdevelopment.assoplus.utils.ApplicationUtils;
 import javafx.beans.property.SimpleBooleanProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.geometry.Pos;
-import javafx.scene.control.*;
+import javafx.scene.control.Button;
+import javafx.scene.control.ContentDisplay;
+import javafx.scene.control.TableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
@@ -21,10 +22,15 @@ import javafx.scene.layout.HBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 import javafx.stage.Window;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import fr.frogdevelopment.assoplus.dto.MemberDto;
+import fr.frogdevelopment.assoplus.service.MembersService;
+import fr.frogdevelopment.assoplus.utils.ApplicationUtils;
 
 import java.io.File;
 import java.util.function.Consumer;
@@ -65,17 +71,18 @@ public class MembersController extends AbstractCustomController {
         ActionCell() {
             hBox.setAlignment(Pos.CENTER);
 
-            ImageView updtateView = new ImageView(new Image("/img/delete_32.png"));
-            hBox.getChildren().add(updtateView);
-            updtateView.setOnMouseReleased(event -> {
+            Button updateBtn = new Button();
+            updateBtn.setGraphic(new ImageView(new Image("/img/edit_user_32.png")));
+            hBox.getChildren().add(updateBtn);
+            updateBtn.setOnAction(event -> {
                 table.getSelectionModel().select(getTableRow().getIndex());
                 addMember(event);
             });
 
-            ImageView deleteView = new ImageView(new Image("/img/delete_32.png"));
-
-            hBox.getChildren().add(deleteView);
-            deleteView.setOnMouseReleased(event -> showYesNoDialog("todo", o -> removeMember()));
+            Button deleteBtn = new Button();
+            deleteBtn.setGraphic(new ImageView(new Image("/img/remove_user_32.png")));
+            hBox.getChildren().add(deleteBtn);
+            deleteBtn.setOnAction(event -> showYesNoDialog("todo", o -> removeMember()));
         }
 
         /** places an add button in the row only if the row is not empty. */
