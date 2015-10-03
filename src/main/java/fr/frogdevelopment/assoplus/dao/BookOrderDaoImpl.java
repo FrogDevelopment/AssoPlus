423b58c73103e68465814e805d4b5971fb2a60cf
Le Gall Benoit <legall.benoit@gmail.com>
2015-10-03 13:33:48 +0200

    add DAOs

--- src/main/java/fr/frogdevelopment/assoplus/dao/BookOrderDaoImpl.java
+++ src/main/java/fr/frogdevelopment/assoplus/dao/BookOrderDaoImpl.java
@@ -6,19 +6,6 @@ package fr.frogdevelopment.assoplus.dao;
 
 import fr.frogdevelopment.assoplus.entities.BookOrder;
 import org.springframework.stereotype.Repository;
-import org.springframework.transaction.annotation.Propagation;
-import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import java.lang.reflect.Field;
-import java.security.AccessController;
-import java.security.PrivilegedAction;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Repository("bookOrderDao")
 public class BookOrderDaoImpl extends CommonDaoImpl<BookOrder> implements BookOrderDao {
